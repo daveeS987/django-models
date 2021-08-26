d75baff021e1b2c4ce02cf4a9808533da4fae058
Davee Sok <daveesok@gmail.com>
2021-08-26 12:47:58 -0500

    pushing any changes, everything currently works

--- snacks/models.py
+++ snacks/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# class Purchaser(models.Model):
-#     pass
-
-
 class Snack(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
